d74c2c84510492d239208a1b63b7beded6b5144c
jackysedy <jacky.sedi@hotmail.com>
2020-05-08 00:04:03 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -11,6 +11,7 @@ do
     TorIP=$(curl -s --socks5-hostname tor-socks-proxy:9150 https://ipecho.net/plain)
     echo "new IP $TorIP"
     if [ $lastIP = $TorIP ]; then
+        echo "Test Fail : last IP = $lastIP   New IP = $TorIP" 
         exit 1
     fi
     lastIP=$TorIP
