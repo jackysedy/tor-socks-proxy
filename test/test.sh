b639d1f91eea04e2425adf5b414295423eff3904
jackysedy <jacky.sedi@hotmail.com>
2020-05-08 11:27:44 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -9,11 +9,10 @@ lastIP=$realIP
 while [[ $(date -u +%s) -le $endtime ]]
 do
     TorIP=$(curl -s --socks5-hostname 127.0.0.1:$PORT https://ipecho.net/plain)
-    echo curl -s --socks5-hostname 127.0.0.1:$PORT https://ipecho.net/plain
     echo "new IP $TorIP"
     if [ "$lastIP" = "$TorIP" ]; then
         echo "Test Fail : last IP = $lastIP New IP = $TorIP" 
-        #exit 1
+        exit 1
     fi
     lastIP=$TorIP
     sleep 10
