23f16fd835d094e9007fc7059992d5be11bab7f2
jackysedy <jacky.sedi@hotmail.com>
2020-05-08 07:13:57 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,13 @@
 sleep 10
 runtime="2 minute"
 endtime=$(date -ud "$runtime" +%s)
-
+CMD="curl -s --socks5-hostname 127.0.0.1:9150 https://ipecho.net/plain"
 realIP=$(curl -s https://ipecho.net/plain)
 echo "Real external IP $realIP" 
 lastIP=$realIP
 while [[ $(date -u +%s) -le $endtime ]]
 do
-    echo curl -s --socks5-hostname 127.0.0.1:9150 https://ipecho.net/plain 
-    echo "$(curl -s --socks5-hostname 127.0.0.1:9150 https://ipecho.net/plain )"
+    echo $CMD 
+    echo "$($CMD)"
     sleep 10
 done
