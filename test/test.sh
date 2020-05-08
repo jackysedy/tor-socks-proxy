c36610dfd2482aade8facaa929b663f0499d4cbe
jackysedy <jacky.sedi@hotmail.com>
2020-05-08 07:59:36 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,19 @@
 sleep 10
 runtime="2 minute"
 endtime=$(date -ud "$runtime" +%s)
-CMD="curl -s --socks5-hostname 127.0.0.1:9150 https://ipecho.net/plain"
+
 realIP=$(curl -s https://ipecho.net/plain)
 echo "Real external IP $realIP" 
 lastIP=$realIP
 while [[ $(date -u +%s) -le $endtime ]]
 do
-    echo $CMD 
-    echo "$($CMD)"
-    sleep 10
-done
+    TorIP=$(curl -s --socks5-hostname tor-socks-proxy:9150 https://ipecho.net/plain)
+    echo "new IP $TorIP"
+    if [ $lastIP = $TorIP ]; then
+        echo "Test Fail : last IP = $lastIP New IP = $TorIP" 
+        exit 1
+    fi
+lastIP=$TorIP
+sleep 10
+Done
+
