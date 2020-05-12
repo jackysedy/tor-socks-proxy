7d38677c13f0b59308c886698db7390394edd7a4
jackysedy <jacky.sedi@hotmail.com>
2020-05-12 02:11:17 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -8,7 +8,7 @@ echo "Real external IP $realIP"
 lastIP=$realIP
 while [[ $(date -u +%s) -le $endtime ]]
 do
-    TorIP=$(curl -s --socks5-hostname loadbalancer-socks-proxy:$PORT https://ipecho.net/plain)
+    TorIP=$(curl -s --socks5-hostname 127.0.0.1:$PORT https://ipecho.net/plain)
     echo "new IP $TorIP"
     if [ "$lastIP" = "$TorIP" ]; then
         echo "Test Fail : last IP = $lastIP New IP = $TorIP" 
